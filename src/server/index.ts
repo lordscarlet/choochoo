4980194f9dba4a619176f636a76696a38591f4fc
Nathan Tate <nathantate88@gmail.com>
2024-11-07 09:40:49 -0800

    Add cors.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import csrf from 'csurf';
 import express, { Request, Response } from 'express';
 import { createServer } from 'http';
@@ -18,6 +19,7 @@ const port = 3000;
 app.use(cookieParser());
 app.use(redisSession);
 app.use(csrf());
+app.use(cors({ origin: 'https://www.choochoo.games' }));
 app.use(express.json());
 app.use(waitForSequelize());
 
