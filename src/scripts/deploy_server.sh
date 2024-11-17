96198d6761d90e1e85921fabde19c18b29d0355b
Nathan Tate <nathantate88@gmail.com>
2024-11-17 10:27:15 -0800

    Add migrations to the deploy script.

--- src/scripts/deploy_server.sh
+++ src/scripts/deploy_server.sh
@@ -1,5 +1,6 @@
 # Step 1: build
 npm run build-server
+cp ./src/scripts/*.sh ./bin/scripts/
 # Step 2: deploy API to server.
 tar -cvzf ./builds/server.tar.gz ./bin
 scp -i ~/Documents/choochoo.pem ./builds/server.tar.gz ec2-user@api.choochoo.games:~/dump/
@@ -9,5 +10,7 @@ ssh -i ~/Documents/choochoo.pem ec2-user@api.choochoo.games << EOF
   rm -rf ~/choochoo/bin
   tar -xzvf ~/dump/server.tar.gz
   npm install
-  pm2 reload index
+  pm2 stop index
+  npm run migrate
+  pm2 start index
 EOF
