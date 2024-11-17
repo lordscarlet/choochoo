65ad13bdf367b8f015a9ae9cb1cb77e7212bcc54
Nathan Tate <nathantate88@gmail.com>
2024-11-17 12:22:56 -0800

    Modify deploy script to invalidate the cloudfront cache.

--- src/client/home/page.tsx
+++ src/client/home/page.tsx
@@ -1,12 +1,12 @@
-import { GameList } from "./game_list";
 import { useMe } from "../services/me";
+import { GameList } from "./game_list";
 
 
 export function HomePage() {
   const user = useMe();
 
   return <div>
-    <h1>Steam Age</h1>
+    <h1>Choo Choo Games</h1>
     <GameList />
   </div>;
 }
--- src/scripts/deploy_client.sh
+++ src/scripts/deploy_client.sh
@@ -3,3 +3,5 @@ npm run build-client
 # Step 2: deploy static files to S3
 aws s3 cp ./src/client/index.html s3://www.choochoo.games/index.html
 aws s3 cp ./dist/ s3://www.choochoo.games/dist/ --recursive
+
+aws cloudfront create-invalidation --distribution-id=E16V74AIRBU7V5 --paths /
