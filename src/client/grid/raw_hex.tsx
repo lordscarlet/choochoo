db0373e83d7ee512a60373fbed2155c409ff5edf
Nathan Tate <nathantate88@gmail.com>
2024-11-19 11:26:57 -0800

    Change the border of a good so they don't merge together.

--- src/client/grid/raw_hex.tsx
+++ src/client/grid/raw_hex.tsx
@@ -193,7 +193,7 @@ function GoodBlock({ center, size, offset, good, highlighted }: { good: Good, ce
 
   const x = center.x - (1.7 * goodSize) + (goodSize * xOffset / 2);
   const y = center.y + (yOffset * goodSize);
-  const stroke = highlighted ? 'yellow' : 'black';
+  const stroke = highlighted ? (good === Good.YELLOW ? 'yellow' : 'black') : (good === Good.BLACK ? 'grey' : 'black');
   return <rect data-good={good} width={goodSize} height={goodSize} x={x} y={y} fill={goodColor(good)} strokeWidth={1} stroke={stroke} />;
 }
 
