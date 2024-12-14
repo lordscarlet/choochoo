600acf58ea7e109e8435d8e891183b9095b834da
Nathan Tate <nathantate88@gmail.com>
2024-12-13 21:41:31 -0800

    Fix the zoom.

--- src/client/grid/game_map.tsx
+++ src/client/grid/game_map.tsx
@@ -207,7 +207,7 @@ export function GameMap() {
   }, [canEmitMove, canEmitBuild, isPending]);
 
   return <>
-    <HexGrid onClick={onClick} allowZoom highlightedTrack={highlightedTrack} clickTargets={clickTargets} selectedGood={selectedGood} grid={grid} />
+    <HexGrid onClick={onClick} allowZoom={true} highlightedTrack={highlightedTrack} clickTargets={clickTargets} selectedGood={selectedGood} grid={grid} />
     <BuildingDialog coordinates={buildingSpace?.coordinates} cancelBuild={() => setBuildingSpace(undefined)} />
   </>;
 }
