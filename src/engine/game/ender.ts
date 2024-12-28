3cc44352648c988fddf4c70ddf1e916c6c68dcaa
Nathan Tate <nathantate88@gmail.com>
2024-12-28 12:21:36 -0800

    Set the end game phase.

--- src/engine/game/ender.ts
+++ src/engine/game/ender.ts
@@ -1,13 +1,18 @@
-import { inject } from "../framework/execution_context";
+import { inject, injectState } from "../framework/execution_context";
 import { GridHelper } from "../map/grid_helper";
+import { Phase } from "../state/phase";
+import { PHASE } from "./phase";
 import { injectGrid } from "./state";
 
 /** This gets run at the end of the game. */
 export class Ender {
   private readonly grid = injectGrid();
   private readonly gridHelper = inject(GridHelper);
+  private readonly phase = injectState(PHASE);
 
   endGame(): void {
+    this.phase.set(Phase.END_GAME);
+
     const danglers = this.grid().getDanglers();
     for (const dangler of danglers) {
       this.gridHelper.setRouteOwner(dangler, undefined);
