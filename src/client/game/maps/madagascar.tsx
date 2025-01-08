5398552eaa51a70ca391321d546cb6cfb377f471
Nathan Tate <nathantate88@gmail.com>
2025-01-07 21:38:15 -0800

    Add the cost of hills to Madagascar's rules.

--- src/client/game/maps/madagascar.tsx
+++ src/client/game/maps/madagascar.tsx
@@ -5,6 +5,7 @@ export function MadagascarRules() {
     <ul>
       <li><b>Bad special actions:</b> all of the actions are worse versions of the base game (Last move, last build, etc.).</li>
       <li><b>Disabled actions:</b> three of the actions are randomly disabled each round.</li>
+      <li><b>Hills (brown hexes):</b> cost $3.</li>
     </ul>
   </div>
 }
