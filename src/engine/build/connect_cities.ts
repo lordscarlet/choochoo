28bc3cf9b2ef9d3ba940e519b13c7f9315800824
Nathan Tate <nathantate88@gmail.com>
2025-01-12 08:31:41 -0800

    Add a log for connecting cities.

--- src/engine/build/connect_cities.ts
+++ src/engine/build/connect_cities.ts
@@ -3,8 +3,10 @@ import { CoordinatesZod } from "../../utils/coordinates";
 import { assert } from "../../utils/validate";
 import { inject, injectState } from "../framework/execution_context";
 import { ActionProcessor } from "../game/action";
+import { Log } from "../game/log";
 import { MoneyManager } from "../game/money_manager";
 import { injectCurrentPlayer, injectGrid } from "../game/state";
+import { City } from "../map/city";
 import { GridHelper } from "../map/grid_helper";
 import { BuilderHelper } from "./helper";
 import { BUILD_STATE } from "./state";
@@ -24,6 +26,7 @@ export class ConnectCitiesAction implements ActionProcessor<ConnectCitiesData> {
   protected readonly currentPlayer = injectCurrentPlayer();
   protected readonly moneyHelper = inject(MoneyManager);
   protected readonly helper = inject(BuilderHelper);
+  protected readonly log = inject(Log);
 
   validate(data: ConnectCitiesData): void {
     const maxTrack = this.helper.getMaxBuilds();
@@ -38,6 +41,7 @@ export class ConnectCitiesAction implements ActionProcessor<ConnectCitiesData> {
   }
 
   process(data: ConnectCitiesData): boolean {
+    const cities = data.connect.map(coordinates => this.grid().get(coordinates) as City);
     const connection = this.grid().findConnection(data.connect)!;
     this.moneyHelper.addMoneyForCurrentPlayer(-connection.cost);
     this.gridHelper.setInterCityOwner(this.currentPlayer().color, connection);
@@ -45,6 +49,8 @@ export class ConnectCitiesAction implements ActionProcessor<ConnectCitiesData> {
     this.buildState.update((buildState) => {
       buildState.buildCount!++;
     });
+
+    this.log.currentPlayer(`connects ${cities.map((city) => city.name()).join(', ')}`);
 
     this.helper.checkOwnershipMarkerLimits();
 
