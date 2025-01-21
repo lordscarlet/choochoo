e2bc53a2a05cb0493298033aaadf64dcfad5f3ac
Nathan Tate <nathantate88@gmail.com>
2025-01-21 07:18:35 -0800

    Fix the migration.

--- src/migrations/20250120162504_nullable_history.ts
+++ src/migrations/20250120162504_nullable_history.ts
@@ -5,7 +5,7 @@ export const up: Migration = async ({ context: queryInterface }) => {
   await queryInterface.changeColumn('GameHistories', 'previousGameData', { type: DataTypes.TEXT, allowNull: true });
   await queryInterface.changeColumn('GameHistories', 'patch', { type: DataTypes.TEXT, allowNull: true });
   await queryInterface.changeColumn('GameHistories', 'actionName', { type: DataTypes.STRING, allowNull: true });
-  await queryInterface.changeColumn('GameHistories', 'actionData', { type: DataTypes.STRING, allowNull: true });
+  await queryInterface.changeColumn('GameHistories', 'actionData', { type: DataTypes.TEXT, allowNull: true });
   await queryInterface.changeColumn('GameHistories', 'userId', { type: DataTypes.INTEGER, allowNull: true });
 }
 
@@ -13,6 +13,6 @@ export const down: Migration = async ({ context: queryInterface }) => {
   await queryInterface.changeColumn('GameHistories', 'previousGameData', { type: DataTypes.TEXT, allowNull: false });
   await queryInterface.changeColumn('GameHistories', 'patch', { type: DataTypes.TEXT, allowNull: false });
   await queryInterface.changeColumn('GameHistories', 'actionName', { type: DataTypes.STRING, allowNull: false });
-  await queryInterface.changeColumn('GameHistories', 'actionData', { type: DataTypes.STRING, allowNull: false });
+  await queryInterface.changeColumn('GameHistories', 'actionData', { type: DataTypes.TEXT, allowNull: false });
   await queryInterface.changeColumn('GameHistories', 'userId', { type: DataTypes.INTEGER, allowNull: false });
 }
