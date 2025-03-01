ce6b24f563799f6212d55c62f89eac3fc2690a1c
Nathan Tate <nathantate88@gmail.com>
2025-03-01 15:50:55 -0800

    Add rules to Disco Inferno.

--- src/maps/disco/rules.tsx
+++ src/maps/disco/rules.tsx
@@ -1,3 +1,29 @@
 export function DiscoInfernoRules() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Same as base game with the following changes:</p>
+      <ul>
+        <li>
+          <b>New Cities:</b> start with cubes
+        </li>
+        <li>
+          <b>Goods growth:</b> is skipped entirely
+        </li>
+        <li>
+          <b>Production:</b> lets you place 2 goods directly in one city.
+        </li>
+        <li>
+          <b>Track:</b> is all built on fire for $3.
+        </li>
+        <li>
+          <b>Chain Reaction:</b> lets you deliver more than one good, as long as
+          the new good starts where the old good was delivered.
+        </li>
+        <li>
+          <b>Burn baby burn:</b> when a city runs out of cubes, it becomes
+          colorless and refuses to accept cubes.
+        </li>
+      </ul>
+    </div>
+  );
 }
