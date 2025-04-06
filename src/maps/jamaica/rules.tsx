30c0dd3c938bf6eaf38c1bba089fb2800ec5f467
Nathan Tate <nathantate88@gmail.com>
2025-04-06 16:35:52 -0700

    Add rules for Jamaica.

--- src/maps/jamaica/rules.tsx
+++ src/maps/jamaica/rules.tsx
@@ -1,3 +1,30 @@
 export function JamaicaRules() {
-  return <p>TBD</p>;
+  return (
+    <div>
+      <p>Same as base game with the following changes:</p>
+      <ul>
+        <li>
+          <b>Dark mountains:</b> cost $5.
+        </li>
+        <li>
+          <b>Production:</b> will not let you place a good of the same color as
+          the city.
+        </li>
+        <li>
+          <b>Turn order:</b> takes place before taking shares (there is no
+          auction). Players alternate going first, with first player paying $5.
+          If the first player cannot or does not want to pay, then the other player
+          can pay $5 to go first instead. If the other player chooses not to, then the
+          first player can go first for free.
+        </li>
+        <li>
+          <b>Game lasts:</b> until there are no goods left on Jamaica. Goods on
+          the growth chart don&apos;t count.
+        </li>
+        <li>
+          <b>Goods growth:</b> rolls 3 dice each turn.
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/maps/st-lucia/rules.tsx
+++ src/maps/st-lucia/rules.tsx
@@ -4,12 +4,14 @@ export function StLuciaRules() {
       <p>Same as base game with the following changes:</p>
       <ul>
         <li>
-          <b>Bid for Turn order:</b> takes place before taking shares. Bid
-          options are $0 or $5.
+          <b>Turn order:</b> takes place before taking shares (there is no
+          auction). Players alternate going first, with first player paying $5.
+          If the first player cannot or does not want to pay, then the other
+          player can pay $5 to go first instead. If the other player chooses not
+          to, then the first player can go first for free.
         </li>
         <li>
-          <b>Turn order pass:</b> lets you bid first. Otherwise, second player
-          bids first.
+          <b>Turn order pass:</b> lets you bid first.
         </li>
         <li>
           <b>Game lasts:</b> for 8 turns.
