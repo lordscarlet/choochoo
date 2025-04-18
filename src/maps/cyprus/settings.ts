82f1299f79560a734d3624179895fda1a2949797
Nathan Tate <nathantate88@gmail.com>
2025-04-18 10:19:45 -0700

    Hook up cyprus costs for the variant. Fixes #72

--- src/maps/cyprus/settings.ts
+++ src/maps/cyprus/settings.ts
@@ -3,7 +3,7 @@ import { MapSettings, ReleaseStage } from "../../engine/game/map_settings";
 import { map } from "./grid";
 import { CyprusAllowedActions } from "./limitted_selection";
 import { CyprusMoveAction } from "./move_goods";
-import { ShortBuild } from "./short_build";
+import { CyprusCostCalculator, ShortBuild } from "./short_build";
 import { CyprusStarter } from "./starter";
 
 export class CyprusMapSettings implements MapSettings {
@@ -16,6 +16,12 @@ export class CyprusMapSettings implements MapSettings {
   readonly stage = ReleaseStage.ALPHA;
 
   getOverrides() {
-    return [CyprusAllowedActions, ShortBuild, CyprusStarter, CyprusMoveAction];
+    return [
+      CyprusAllowedActions,
+      ShortBuild,
+      CyprusStarter,
+      CyprusMoveAction,
+      CyprusCostCalculator,
+    ];
   }
 }
