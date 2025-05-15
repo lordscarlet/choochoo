b95cc3591befdd4b4421b5aeec3f4467bbbd4397
Nathan Tate <nathantate88@gmail.com>
2025-05-15 08:59:21 -0700

    Clarify the end-game rules for Jamaica. Fixes #82.

--- src/maps/jamaica/rules.tsx
+++ src/maps/jamaica/rules.tsx
@@ -13,13 +13,14 @@ export function JamaicaRules() {
         <li>
           <b>Turn order:</b> takes place before taking shares (there is no
           auction). Players alternate going first, with first player paying $5.
-          If the first player cannot or does not want to pay, then the other player
-          can pay $5 to go first instead. If the other player chooses not to, then the
-          first player can go first for free.
+          If the first player cannot or does not want to pay, then the other
+          player can pay $5 to go first instead. If the other player chooses not
+          to, then the first player can go first for free.
         </li>
         <li>
-          <b>Game lasts:</b> until there are no goods left on Jamaica. Goods on
-          the growth chart don&apos;t count.
+          <b>Game ends:</b> at the end of the first turn where there are no
+          goods left on Jamaica. Goods on the growth chart don&apos;t count.
+          Reminder: this check occurs <i>after</i> goods growth.
         </li>
         <li>
           <b>Goods growth:</b> rolls 3 dice each turn.
