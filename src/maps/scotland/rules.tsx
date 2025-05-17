081a54d5dce19eac3f63f325eb6ed1335918ae1f
Grimiku <kanciok01@yahoo.pl>
2025-05-17 13:49:49 +0200

    Adding rules description

--- src/maps/scotland/rules.tsx
+++ src/maps/scotland/rules.tsx
@@ -3,7 +3,21 @@ export function ScotlandRules() {
     <div>
       <p>Same as base game with the following changes:</p>
       <ul>
-        <li>TODO: Add rules</li>
+        <li>
+          <b>Mountains with a river:</b> cost $5 to build over.
+        </li>
+        <li>
+          <b>Ferry links:</b> the ferry connections (Stornoway-Ullapool & Belfast-Ayr) cost $6 and can only be built once both of respective towns on each side are urbanized.
+        </li>
+        <li>
+          <b>Turn Order action:</b> skips auction entirely on next turn giving priority to the player who has taken that action.
+        </li>
+        <li>
+          <b>Goods growth:</b> rolls 4 dice each turn.
+        </li>
+        <li>
+          <b>Game ends:</b> after 8 turns.
+        </li>
       </ul>
     </div>
   );
