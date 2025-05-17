3b053f9f383e1365ed70647273e18798ed0575b7
Nathan Tate <nathantate88@gmail.com>
2025-05-17 10:35:37 -0700

    Remove log of sequelize.

--- src/server/sequelize.ts
+++ src/server/sequelize.ts
@@ -1,28 +1,27 @@
 import Sequelize from "@sequelize/core";
 import { PostgresDialect } from "@sequelize/postgres";
 import { NextFunction, Request, Response } from "express";
+import { logError } from "../utils/functions";
 import { FeedbackDao } from "./feedback/dao";
 import { GameDao } from "./game/dao";
 import { GameHistoryDao } from "./game/history_dao";
 import { LogDao } from "./messages/log_dao";
 import { UserDao } from "./user/dao";
 import { environment } from "./util/environment";
-import { log, logError } from "../utils/functions";
 
-export const sequelize = new Sequelize({ 
+export const sequelize = new Sequelize({
   dialect: PostgresDialect,
   url: environment.postgresUrl.toString(),
-  logging: log,
+  // logging: log,
   models: [GameDao, UserDao, LogDao, GameHistoryDao, FeedbackDao],
 });
 
 const connection = sequelize.authenticate();
 
-connection
-  .catch((err: unknown) => {
-    logError("failed to connect to sql database", err);
-    process.exit();
-  });
+connection.catch((err: unknown) => {
+  logError("failed to connect to sql database", err);
+  process.exit();
+});
 
 export function waitForSequelize() {
   return (req: Request, res: Response, next: NextFunction): void => {
