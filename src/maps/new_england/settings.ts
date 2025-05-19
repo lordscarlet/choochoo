7b8435bdb31b4058793cff545a79061230330698
Nathan Tate <nathantate88@gmail.com>
2025-05-19 09:05:27 -0700

    Remove Turn order pass from New England map

--- src/maps/new_england/settings.ts
+++ src/maps/new_england/settings.ts
@@ -5,7 +5,9 @@ import {
   Rotation,
 } from "../../engine/game/map_settings";
 import { Module } from "../../engine/module/module";
+import { Action } from "../../engine/state/action";
 import { Phase } from "../../engine/state/phase";
+import { AvailableActionsModule } from "../../modules/available_actions";
 import { CompleteLinkBuldModule } from "../../modules/complete_link_build";
 import { PhasesModule } from "../../modules/phases";
 import { remove } from "../../utils/functions";
@@ -38,6 +40,7 @@ export class NewEnglandMapSettings implements MapSettings {
         replace: (oldPhases) => remove(oldPhases, Phase.TURN_ORDER),
       }),
       new CompleteLinkBuldModule(),
+      new AvailableActionsModule({ remove: [Action.TURN_ORDER_PASS] }),
     ];
   }
 }
