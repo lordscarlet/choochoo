2ad5995b91f4fdd03523a429f2aa5182162ed336
Nathan Tate <nathantate88@gmail.com>
2025-06-03 10:22:15 -0700

    Fix Berlin cubes.

--- src/maps/germany/settings.ts
+++ src/maps/germany/settings.ts
@@ -1,5 +1,9 @@
 import { GameKey } from "../../api/game_key";
+import { inject } from "../../engine/framework/execution_context";
 import { MapSettings, ReleaseStage } from "../../engine/game/map_settings";
+import { GoodsHelper } from "../../engine/goods_growth/helper";
+import { GoodsGrowthPhase } from "../../engine/goods_growth/phase";
+import { goodToString } from "../../engine/state/good";
 import { interCityConnections } from "../factory";
 import {
   GermanyBuildAction,
@@ -31,6 +35,24 @@ export class GermanyMapSettings implements MapSettings {
       GermanyBuilderHelper,
       GermanyBuildAction,
       GermanyBuildPhase,
+      GermanyGoodsGrowth,
     ];
+  }
+}
+
+export class GermanyGoodsGrowth extends GoodsGrowthPhase {
+  protected readonly helper = inject(GoodsHelper);
+
+  onStart(): void {
+    super.onStart();
+    const berlin = [...this.grid.findAllCities()].find(
+      (city) => city.data.name === "Berlin",
+    )!;
+    const currentBerlinGoods = berlin.getGoods();
+    const newGood = this.helper.drawGood();
+    this.grid.update(berlin.coordinates, (cityData) => {
+      cityData.goods = [...currentBerlinGoods, newGood];
+    });
+    this.log.log(`A ${goodToString(newGood)} good is added to Berlin (J9)`);
   }
 }
