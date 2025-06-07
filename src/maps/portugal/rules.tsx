7335280ddf99804dad36eb214e2475c507bacff3
Nathan Tate <nathantate88@gmail.com>
2025-06-07 11:28:40 -0700

    Add rules to Portugal

--- src/maps/portugal/rules.tsx
+++ src/maps/portugal/rules.tsx
@@ -3,7 +3,18 @@ export function PortugalRules() {
     <div>
       <p>Same as base game with the following changes:</p>
       <ul>
-        <li>TODO: Add rules</li>
+        <li>
+          <b>Lisboa:</b> You can only build one link out of Lisboa per turn
+          (incl. sea routes).
+        </li>
+        <li>
+          <b>Sea links:</b> may be claimed at the cost of $6. Some links may be
+          claimed by multiple players.
+        </li>
+        <li>
+          <b>Açores and Madeira:</b> receive one random cube each goods growth
+          phase.
+        </li>
       </ul>
     </div>
   );
