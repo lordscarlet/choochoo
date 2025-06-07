de64857d475150f3a4f3020aae04ebc5159fa1de
Nathan Tate <nathantate88@gmail.com>
2025-06-07 12:03:59 -0700

    Add rivers to Portgugal.

--- src/maps/portugal/view_settings.ts
+++ src/maps/portugal/view_settings.ts
@@ -1,5 +1,5 @@
-import { Rotation } from "../../engine/game/map_settings";
 import { MapViewSettings } from "../view_settings";
+import { PortugalRivers } from "./rivers";
 import { PortugalRules } from "./rules";
 import { PortugalMapSettings } from "./settings";
 
@@ -7,6 +7,8 @@ export class PortugalViewSettings
   extends PortugalMapSettings
   implements MapViewSettings
 {
-  rotation = Rotation.CLOCKWISE;
+  // rotation = Rotation.CLOCKWISE;
   getMapRules = PortugalRules;
+
+  getTexturesLayer = PortugalRivers;
 }
