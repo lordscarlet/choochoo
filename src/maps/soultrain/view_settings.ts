0a9ca109be9c774d33ab949e8bd3ab5835c7023d
Nathan Tate <nathantate88@gmail.com>
2025-06-10 07:46:46 -0700

    Add an action description to engineer in Soul Train.

--- src/maps/soultrain/view_settings.ts
+++ src/maps/soultrain/view_settings.ts
@@ -1,3 +1,4 @@
+import { Action } from "../../engine/state/action";
 import { MapViewSettings } from "../view_settings";
 import { SoulTrainRules } from "./rules";
 import { SoulTrainMapSettings } from "./settings";
@@ -7,4 +8,10 @@ export class SoulTrainViewSettings
   implements MapViewSettings
 {
   getMapRules = SoulTrainRules;
+
+  getActionDescription(action: Action): string | undefined {
+    if (action == Action.ENGINEER) {
+      return "Cut the total cost of your build in half (rounded up).";
+    }
+  }
 }
