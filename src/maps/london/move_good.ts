996ff2d4d25e06f7e143c2cc707ca6187e3b3190
Nathan Tate <nathantate88@gmail.com>
2025-06-17 20:11:41 -0700

    city in london move data is optional when starting from a non-city

--- src/maps/london/move_good.ts
+++ src/maps/london/move_good.ts
@@ -7,9 +7,11 @@ import { OnRollData } from "../../engine/state/roll";
 import { Good, goodToString } from "../../engine/state/good";
 import { Random } from "../../engine/game/random";
 import { CoordinatesZod } from "../../utils/coordinates";
+import { City } from "../../engine/map/city";
+import { Space } from "../../engine/map/grid";
 
 export const LondonMoveData = MoveData.extend({
-  city: CoordinatesZod,
+  city: CoordinatesZod.optional(),
 });
 export type LondonMoveData = z.infer<typeof LondonMoveData>;
 
@@ -21,9 +23,21 @@ export class LondonMoveAction extends MoveAction<LondonMoveData> {
   validate(data: LondonMoveData) {
     super.validate(data);
 
+    const start = this.grid().get(data.startingCity);
+    if (!(start instanceof City)) {
+      assert(data.city === undefined, {
+        invalidInput: "city should be empty when starting from a town",
+      });
+      return;
+    }
+
+    assert(data.city !== undefined, {
+      invalidInput: "city must be set and non-empty",
+    });
+
     const city = this.gridHelper.lookup(data.city);
     assert(city !== undefined, {
-      invalidInput: "city must be set and non-empty",
+      invalidInput: "city coordinate is invalid",
     });
 
     assert(
@@ -38,7 +52,14 @@ export class LondonMoveAction extends MoveAction<LondonMoveData> {
   process(action: LondonMoveData): boolean {
     const result = super.process(action);
 
-    const city = this.gridHelper.lookup(action.city);
+    let city: Space | undefined;
+    if (action.city === undefined) {
+      city = this.gridHelper.lookup(
+        action.path[action.path.length - 1].endingStop,
+      );
+    } else {
+      city = this.gridHelper.lookup(action.city);
+    }
     assert(city !== undefined);
 
     const coordinates = city.coordinates;
