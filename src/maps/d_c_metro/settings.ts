de55fc19019b37bab698303b44e97f7fbaa867e5
Nathan Tate <nathantate88@gmail.com>
2025-06-19 16:26:14 -0700

    Set DC Metro to Alpha.

--- src/maps/d_c_metro/settings.ts
+++ src/maps/d_c_metro/settings.ts
@@ -13,7 +13,7 @@ export class DCMetroMapSettings implements MapSettings {
   readonly minPlayers = 3;
   readonly maxPlayers = 3;
   readonly startingGrid = map;
-  readonly stage = ReleaseStage.DEVELOPMENT;
+  readonly stage = ReleaseStage.ALPHA;
 
   getOverrides() {
     return [
