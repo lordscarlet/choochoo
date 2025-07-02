c2678e7a79a244d4924d69670d6b8ee8c18f6fd0
Ian Haken <ihaken@netflix.com>
2025-07-02 14:39:09 -0700

    Move denmark map to alpha.

--- src/maps/denmark/settings.ts
+++ src/maps/denmark/settings.ts
@@ -74,7 +74,7 @@ export class DenmarkMapSettings implements MapSettings {
     { connects: ["Nykøbing", "Warnemünde"], cost: 4, center: [15, 4] },
     { connects: ["Nykøbing", "Warnemünde"], cost: 6, center: [15, 3] },
   ]);
-  readonly stage = ReleaseStage.DEVELOPMENT;
+  readonly stage = ReleaseStage.ALPHA;
   readonly rotation = Rotation.CLOCKWISE;
 
   getOverrides() {
