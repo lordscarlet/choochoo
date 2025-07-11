b598913f3bb6d2404241fc9c02459211f93acf03
Emil Mossberg <emil.mossberg@vaimo.com>
2025-07-11 05:49:32 +0200

    Safety commit

--- src/maps/poland/settings.ts
+++ src/maps/poland/settings.ts
@@ -9,12 +9,12 @@ import { map } from "./grid";
 export class PolandMapSettings implements MapSettings {
   readonly key = GameKey.POLAND;
   readonly name = "Poland";
-  readonly minPlayers = 3;
-  readonly maxPlayers = 4;
+  readonly minPlayers = 3; 
+  readonly maxPlayers = 6;
   readonly startingGrid = map;
   readonly stage = ReleaseStage.DEVELOPMENT;
-  readonly designer = "TO DO What do add here";
-  readonly implementerId = JACK; // TO DO change this
+  readonly designer = "John Bohrer";
+  readonly implementerId = JACK; // TO DO : change this
 
   getOverrides() {
     return [];
