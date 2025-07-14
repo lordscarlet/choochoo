8c3bab92b31764efc1c4ea360cc413a3b0ac6718
Grimiku <kanciok01@yahoo.pl>
2025-07-14 21:06:39 +0200

    [Portugal] Small wording correction

--- src/maps/portugal/rules.tsx
+++ src/maps/portugal/rules.tsx
@@ -9,7 +9,7 @@ export function PortugalRules() {
         </li>
         <li>
           <b>Sea links:</b> may be claimed at the cost of $6. Some links may be
-          claimed by multiple players. Track may exit through Sines and Sagres to connect with sea links without the two towns needing to be urbanized.
+          claimed by multiple players. Track may pass through Sines and Sagres to connect with sea links without the two towns needing to be urbanized.
         </li>
         <li>
           <b>Açores and Madeira:</b> receive one random cube each goods growth
